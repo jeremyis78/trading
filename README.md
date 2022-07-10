ba7af87f2426257146dcc255666785e23b725abe
Jeremy Brooks <jeremyis@live.com>
2022-07-09 23:56:09 -0700

    include proper CSV support with --csv option * remove irrelavant and commented code

--- README.md
+++ README.md
@@ -3,10 +3,11 @@
 A simple repository of helpful trading tools.
 
 ## oa-positions-parser.py
-This script is for those users with an [OptionAlpha](https://optionalpha.com) subscription who would like to download the
-trades their bot or bots have made into Excel or Sheets. Several of us are eagerly awaiting a Download
+This script is for those users with an [OptionAlpha](https://optionalpha.com) subscription who would like to 
+download the trades their bot or bots have made into Excel or Sheets. Several of us are eagerly awaiting a Download
 button on the Positions page but until then you can use this script.  If you trade options systematically
-and don't have an account and want to automate your option trades, check them out!
+and don't have an account and want the help of automating much of the mundane parts of your option 
+trading, check them out!
 
 Additional prerequisites before you can run this script:
 
@@ -31,28 +32,37 @@ C) You'll need to produce the HTML file to pass to the script.
   1) Visit your bot's positions page (e.g. https://app.optionalpha.com/bots/bot/BOTXXXXX/positions),
   2) Scroll down and click "Load More" until all positions are on screen
   3) Then save that page as an html file (File|Save Page as...). 
-  4) Choose a filename with no spaces (maybe save as the name-of-your-bot.html) to make things easier later when you run the script.
+  4) Choose a filename with no spaces (maybe save as the my-bot-name.html) to make things easier later when you run the script.
 Note: there's a [bug in Chrome](https://support.google.com/chrome/thread/4239329/chrome-webpage-save-as-for-webpage-html-only-option-no-longer-works?hl=en)
 where you must save as "Webpage, Complete" in order to get the HTML file saved 
 properly (you can't use "Webpage, HTML Only" until they fix the bug). 
-Note: since this is a "screen scraper" script the only data available is what's visible on that positions page. For instance,
-we only get the close date of the trade--there's no close time or opening date/time--so the script can't compute things like 
-Days or Minutes in Trade.
+Note: since this is a "screen scraper" script the only data available is what's visible
+on that positions page. For instance, there's no opening date of the trade (or times)
+on that page.
 
 
 ### Usage
 python oa-positions-parser.py -h
-```usage: oa-positions-parser.py [-h] file
+```usage: oa-positions-parser.py [-h] [--csv] file
 
-Parses the closed positions found on the positions page of an OptionAlpha bot (produces TSV output suitable for import to Excel or Sheets).
+Parses the closed positions found on the positions page of an OptionAlpha bot.
 
 positional arguments:
-  file        HTML file saved from OA bots positions page
+file        HTML file saved from OA bots positions page
 
 optional arguments:
-  -h, --help  show this help message and exit
+-h, --help  show this help message and exit
+--csv       Output CSV instead of the default TSV (tab-separated values)
 
-Example: python tc0dte-positions.html
+Examples (copies script output to clipboard):
+(Windows): python oa-positions-parser.py %USERPROFILE%/Downloads/my-bot-name.html|clip
+or
+(MAC): python oa-positions-parser.py ~/Downloads/my-bot-name.html|pbcopy
+
+Then open Excel/Sheets and paste.  Done.
+
+You can also export as CSV but it doesn't play as nice with Excel or Sheets by default:
+python oa-positions-parser.py --csv ~/Downloads/my-bot-name.html
 ```
 
 ### Sample usage showing TSV output
@@ -96,4 +106,7 @@ If you're unfamiliar with running commands like this, try these useful articles:
 Open Terminal on Mac: https://support.apple.com/guide/terminal/open-or-quit-terminal-apd5265185d-f365-44cb-8b09-71a064a42125/mac
 
 Open Terminal on Windows: https://docs.microsoft.com/en-us/windows/terminal/
-Open CMD on Windows: https://www.lifewire.com/how-to-open-command-prompt-2618089 
+Open CMD on Windows: https://www.lifewire.com/how-to-open-command-prompt-2618089
+
+clip: https://docs.microsoft.com/en-us/windows-server/administration/windows-commands/clip
+pbcopy: https://osxdaily.com/2007/03/05/manipulating-the-clipboard-from-the-command-line/
--- oa-positions-parser.py
+++ oa-positions-parser.py
@@ -1,18 +1,42 @@
 
 from bs4 import BeautifulSoup
-import sys
 from lxml import etree
-from decimal import Decimal
 import argparse
+import csv
+import sys
  
-msg = "Parses the closed positions found on the positions page of an OptionAlpha bot (produces TSV output suitable for import to Excel or Sheets)."
-epilog_msg = "Example:  python tc0dte-positions.html"
+msg = "Parses the closed positions found on the positions page of an OptionAlpha bot."
+epilog_msg = """usage: oa-positions-parser.py [-h] [--csv] file
+
+Parses the closed positions found on the positions page of an OptionAlpha bot.
+
+positional arguments:
+  file        HTML file saved from OA bots positions page
+
+optional arguments:
+  -h, --help  show this help message and exit
+  --csv       Output CSV instead of the default TSV (tab-separated values)
+
+Examples (copies script output to clipboard):
+
+(Windows): python oa-position-parser.py %USERPROFILE%/Downloads/my-bot-name.html|clip
+or
+(MAC): python oa-position-parser.py ~/Downloads/my-bot-name.html|pbcopy
+
+Then open Excel/Sheets and paste.  Done.
+
+You can also export as CSV but it doesn't play as nice with Excel or Sheets by default:
+python oa-position-parser.py --csv ~/Downloads/my-bot-name.html
+"""
 parser = argparse.ArgumentParser(description=msg, epilog=epilog_msg,
-                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-#parser.add_argument("-c", "--commissions", help="Commissions per contract", default=0)
+                                 formatter_class=argparse.RawTextHelpFormatter)
+parser.add_argument("--csv", help="Output CSV instead of the default TSV (tab-separated values)", action="store_true")
 parser.add_argument("file", help="HTML file saved from OA bots positions page")
+
 args = parser.parse_args()
 config = vars(args)
+
+delim = ',' if args.csv else '\t'
 
 chars_to_remove = [u' ', u'$', u',']
 
@@ -20,21 +44,26 @@ def remove_chars(subj, chars):
     sc = set(chars)
     return ''.join([c for c in subj if c not in sc])
 
-commpercontract = 0   # Decimal(config['commissions'])
-with open(config['file']) as fp:
+with open(args.file) as fp:
     soup = BeautifulSoup(fp, 'html.parser')
     dom = etree.HTML(str(soup))
     #bot name is under the <a class="edit-title"> of the SECOND <h1 class="title">
-    botH1s = soup.find_all("h1", class_="title") #xpath from Chrome Dev Tools: '//*[@id=bots-bot]/hd/ct[2]/h1/a'
-    bot = botH1s[1].find("a", class_="edit-title").string
+    h1s = soup.find_all("h1", class_="title") #xpath from Chrome Dev Tools: '//*[@id=bots-bot]/hd/ct[2]/h1/a'
+    if h1s and len(h1s) == 2:
+        a = h1s[1].find("a", class_="edit-title")
+        bot = a.string if a else ""
+    else:
+        bot = ""
     closedpos = soup.find(id="bots-bot-positions-closedpos")
     bd = closedpos.find_all('bd', class_="dim-scroller")
     rows = bd[0].find_all("row", class_="pos")
-    #print("tradeno\tbot\tsym\texp\tstrat\tpostext\tstatus\tclosedate\tqty\tcost\tcostdesc\tcomm\tpnl")
-    print("tradeno\tbot\tsym\texp\tstrat\tpostext\tstatus\tclosedate\tqty\tcost\tcostdesc\tpnl")
+
+    writer = csv.writer(sys.stdout, delimiter=delim,
+                        quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    writer.writerow(["tradeno", "bot", "sym", "exp", "strat", "postext", "status", "closedate", "qty",
+                    "cost", "costdesc", "pnl"])
     tradeno = 1;
     for row in rows:
-        #print(type(pos.bd.div.div))
         symdiv = row.bd.find("div", class_="symbol")
         sym = symdiv.find("span", class_="sym").string
         exp = symdiv.find("span", class_="exp").string
@@ -54,24 +83,6 @@ with open(config['file']) as fp:
 
         pnldiv = row.bd.find("div", class_="pnl")
         pnl = remove_chars(pnldiv.string, chars_to_remove)   # You'd need to strip commas and $ character to compute commissions
-         
-        if "Canceled" == status: continue  # Skip Canceled orders
 
-        strat_qty = 0  #contract quantity for a strategy
-        if 'Spread' in strat:
-            strat_qty = 2
-        elif 'Iron' in strat:
-            strat_qty = 4
-        elif 'Long Call' in strat or 'Long Put' in strat:
-            strat_qty = 1
-
-        # Assuming commissions for both open and close positions
-        # Expired positions are not accounted for so comms will be generated as if the there was a closing trade (todo: could fix this)
-        # Equity positions assume 0 commissions
-        # No ORF/SEC/TRF,etc fees are included in this amount
-        #Note: no commissions are calculated currently; can't get a number extracted from beautifulsoup; what I am doing wrong?
-        comm = Decimal(strat_qty) * Decimal(qty) * Decimal(2) * commpercontract
-        #netpnl = Decimal(pnl) - comm
-        print("{:d}\t{:s}\t{:s}\t{:s}\t{:s}\t{:s}\t{:s}\t{:s}\t{:s}\t{:s}\t{:s}\t{:s}"
-              .format(tradeno, bot, sym, exp, strat, postext, status, closedate, qty, cost, costdesc, pnl))
+        writer.writerow([tradeno, bot, sym, exp, strat, postext, status, closedate, qty, cost, costdesc, pnl])
         tradeno += 1
