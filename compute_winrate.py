733986f1d9d892d47cb547a60a92323cec9e01eb
Jeremy Brooks <jeremyis@live.com>
2024-10-01 21:21:52 -0700

    include example output

--- compute_winrate.py
+++ compute_winrate.py
@@ -5,8 +5,17 @@ import sys
 # . venv/bin/activate
 # pip install pandas
 # python compute_winrate.py <file>
+#
+# To deactivate virtualenv:
+# deactivate
 
-# Example: python compute_winrate.py ~/Downloads/BOT92831316391850954728831-20231020-d02fbd.csv
+# Example: This is Lee's AUTO UP DB9 bot as of 2024-05-25
+# (venv) ➜  $ python compute_winrate.py ~/Downloads/BOT_AUTO_UP_DB9-20240525-7a7a1b.csv
+# type  wins  num_trades  avg_daysInTrade    min_trade_date    max_trade_date     avg_PnL  sum_PnL  win_rate
+# 0          longput    75         184        20.711957  01-12-2023 10:15  12-15-2023 16:15  -17.983696    -3309  0.407609
+# 1    longputspread    46          59         9.745763  01-14-2022 11:45  12-22-2022 12:45   28.949153     1708  0.779661
+# 2  shortcallspread    73          74         9.716216  01-14-2022 11:45  12-28-2022 11:16  600.810811    44460  0.986486
+# 3   shortputspread     5           6        34.000000  03-10-2023 13:00   12-15-2022 9:45   72.500000      435  0.833333
 
 def compute_win_rate(file):
     """
